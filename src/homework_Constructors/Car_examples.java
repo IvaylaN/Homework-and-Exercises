3c186f32df87237e43dfab5a06eaac1c281de7b4
IvaylaN <132487990+IvaylaN@users.noreply.github.com>
2023-06-04 17:50:33 +0300

    new homework Exceptions

---
+++ src/homework_Constructors/Car_examples.java
@@ -0,0 +1,11 @@
+package homework_Constructors;
+
+public class Car_examples {
+    public static void main(String[] args) {
+        Car car1 = new Car ("Audi", "black", 2020, 1200, true);
+        Car car2 = new Car ("Marcedes", "red", true);
+        Car car3 = new Car ("VW", "blue", 2015, 1200);
+        Car car4 = new Car ("Pegeout", 2010, 1000, true);
+        Car car5 = new Car ();
+    }
+}
---
+++ src/homework_Exceptions/Exercises.java
@@ -0,0 +1,22 @@
+package homework_Exceptions;
+
+import java.util.Scanner;
+
+public class Exercises {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please provide the first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.println("Please provide the second number: ");
+        double num2 = scanner.nextDouble();
+        System.out.println("Result after division:" + divideNumbers(num1, num2));
+    }
+    public static double divideNumbers(double x, double y) {
+        try {
+            return x / y;
+        } catch (ArithmeticException e) {
+            throw new RuntimeException("Cannot divide by 0!");
+        }
+    }
+}
+
